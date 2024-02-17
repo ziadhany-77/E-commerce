b506cf0209a10be11f356e2fb88eb83d7d46fdaf
ziad <zizo.juv7@gmail.com>
2024-02-17 21:16:45 +0200

    webhook

--- Src/bootstrap.js
+++ Src/bootstrap.js
@@ -2,8 +2,40 @@ import express from 'express'
 import morgan from 'morgan'
 import { AppError } from './utils/errorhandeling.js'
 import v1Router from './routers/v1.route.js'
+import Stripe from 'stripe'
 
 const bootstrap = (app) => {
+
+	app.post(
+		'/webhook',
+		express.raw({ type: 'application/json' }), (request, response) => {
+		const sig = request.headers['stripe-signature'];
+	
+		let event;
+	
+		try {
+			event = Stripe.webhooks.constructEvent(
+				request.body,
+				sig,
+				process.env.STRIPE_WEBHOOK_SECRET
+			);
+		} catch (err) {
+			response.status(400).send(`Webhook Error: ${err.message}`);
+			return;
+		}
+	
+		switch (event.type) {
+			case 'checkout.session.completed':
+				const data = event.data.object;
+				console.log({data})
+			break;
+			default:
+			console.log(`Unhandled event type ${event.type}`);
+		}
+	
+		response.send();
+	});
+
     app.use(express.json())
 	
     app.use(morgan('dev'))
